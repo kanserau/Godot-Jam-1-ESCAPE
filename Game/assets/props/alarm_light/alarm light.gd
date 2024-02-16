4b28103f76931898fa4b371f15c2ab1ae788814b
NarisR <nariscatboy@gmail.com>
2024-02-16 15:15:54 +1100

    add spinning light and modular pipes

---
+++ Game/assets/props/alarm_light/alarm light.gd
@@ -0,0 +1,10 @@
+extends Node3D
+
+# TODO:
+# @export var color -> on set override Geam
+
+# TODO: animation -> rotate Meta + Lumina + spot light
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.

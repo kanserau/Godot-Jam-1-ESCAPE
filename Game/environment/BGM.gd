b5e684cf66dae0960b51ba7241419c3d1d6b7cba
HamRanpara <gyradoshr@gmail.com>
2024-02-17 13:59:29 +0200

    How to use event signal

--- Game/environment/BGM.gd
+++ Game/environment/BGM.gd
@@ -1,9 +1,10 @@
 extends AudioStreamPlayer
 
-
+const GameTypes = preload("res://resources/templates/types.gd")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	SettingsManager.connect_bgm_volume_change_listener(self, "on_volume_changed")
+	#GameManager.connect_to_signal(self, "event_triggered", on_event_triggered)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,3 +13,9 @@ func _process(_delta):
 
 func on_volume_changed(new_volume_db):
 	self.volume_db = new_volume_db
+
+#func on_event_triggered(event: GameEvent):
+	#self.pla
+	#if event.event == GameTypes.Events.FIRE:
+		#play fire voice line
+#	pass
